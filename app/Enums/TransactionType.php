bb7ed5fada71a6fd10ee8f1dd9056ee83f03b56b
His Imre Sándor <his.imre.sandor@gmail.com>
2024-04-04 23:01:06 +0200

    Add bet and win to TransactionType enum

--- app/Enums/TransactionType.php
+++ app/Enums/TransactionType.php
@@ -6,12 +6,16 @@ enum TransactionType: string
 {
     case DEPOSIT = 'deposit';
     case WITHDRAW = 'withdraw';
+    case BET = 'bet';
+    case WIN = 'win';
 
     public static function getValues(): array
     {
         return [
             self::DEPOSIT,
             self::WITHDRAW,
+            self::BET,
+            self::WIN,
         ];
     }
 }
