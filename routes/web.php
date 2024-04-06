3a8f4e6f8b2b60a455fbb1bf4cc99de5af5d9f91
His Imre Sándor <his.imre.sandor@gmail.com>
2024-04-06 21:56:47 +0200

    Group routes

--- routes/web.php
+++ routes/web.php
@@ -9,15 +9,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', [PageController::class, 'index'])->middleware(['auth', 'verified'])->name('index');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('balance', [BalanceController::class, 'edit'])->name('balance.edit');
-    Route::post('/balance/bet', [BalanceController::class, 'bet'])->name('balance.bet');
-    Route::post('/balance/win', [BalanceController::class, 'win'])->name('balance.win');
-    Route::post('/balance/deposit', [BalanceController::class, 'deposit'])->name('balance.deposit');
-    Route::post('/balance/withdraw', [BalanceController::class, 'withdraw'])->name('balance.withdraw');
+
+    Route::controller(ProfileController::class)->prefix('/profile')->name('profile.')->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::patch('/', 'update')->name('update');
+        Route::delete('/', 'destroy')->name('destroy');
+    });
+
+    Route::controller(BalanceController::class)->prefix('/balance')->name('balance.')->group(function () {
+        Route::get('/', 'edit')->name('edit');
+        Route::post('/bet', 'bet')->name('bet');
+        Route::post('/win', 'win')->name('win');
+        Route::post('/deposit', 'deposit')->name('deposit');
+        Route::post('/withdraw', 'withdraw')->name('withdraw');
+    });
+
     Route::post('/game', [GameController::class, 'store'])->name('game.store');
+
 });
 
 require __DIR__.'/auth.php';
