a4eeb3d86c3bff12655254e34a2f4b32a58561b9
His Imre Sándor <his.imre.sandor@gmail.com>
2024-04-06 23:03:39 +0200

    Save card ids instead of Card model in Game

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -10,8 +10,22 @@ class GameController extends Controller
 {
     public function store(StoreRequest $request) : RedirectResponse {
         $game = $request->validated();
+        $player_cards = [];
+        $dealer_cards = [];
+        $deck = [];
+        foreach ($game['player_cards'] as $card) {
+            $player_cards[] = $card['id'];
+        }
+        foreach ($game['dealer_cards'] as $card) {
+            $dealer_cards[] = $card['id'];
+        }
+        foreach ($game['deck'] as $card) {
+            $deck[] = $card['id'];
+        }
+        $game['player_cards'] = $player_cards;
+        $game['dealer_cards'] = $dealer_cards;
+        $game['deck'] = $deck;
         $game['user_id'] = auth()->id();
-        // dd($game);
         Game::create($game);
         return redirect()->back();
     }
